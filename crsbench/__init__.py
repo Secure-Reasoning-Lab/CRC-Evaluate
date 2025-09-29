d5c6d238e34275be21ed2bc1207cd68dd08d4897
Yu-Fu Fu <yufu@yfu.tw>
2025-09-29 07:35:22 -0400

    [utils] add utils and make all module under crsbench

--- crsbench/__init__.py
+++ crsbench/__init__.py
@@ -3,3 +3,8 @@
 __version__ = "0.1.0"
 __author__ = "CRSBench Team"
 __description__ = "A benchmark suite for evaluating Cyber Reasoning Systems"
+
+# Make submodules available
+from . import utils
+from . import migration
+from . import hint_generation
